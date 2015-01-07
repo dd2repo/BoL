d4f34a08ea46314bfd34e0bd4153260275bb5c26
dd2repo <yannicklanzrath@googlemail.com>
2015-01-07 13:32:38 +0100

    Update and rename WobbleWukong.lua to deadwukong.lua

--- deadwukong.lua
+++ deadwukong.lua
@@ -1,15 +1,3 @@
---[[
-Wooble Wukong
-by DeadDevil2 :)
-
-Chanelog:
-v1.0 inital release
-v1.1 Fixed some target bugs 
-v1.2 Added Auto Ultimate
-v.1.3 Fixed ult cancel bug
-v.1.4 Fixed ult cancel bug..... hopefully
-]]
-
 if myHero.charName ~= "MonkeyKing" then
 return
 end
@@ -19,9 +7,9 @@ require 'Vprediction'
 
 local selectedTar = nil
 local VP = nil
-local version = 1.3
+local version = 1.0
 local AUTOUPDATE = true
-local SCRIPT_NAME = "WobbleWukong"
+local SCRIPT_NAME = "deadwukong"
 local selectedTar = nil
 local SOURCELIB_URL = "https://raw.github.com/TheRealSource/public/master/common/SourceLib.lua"
 local SOURCELIB_PATH = LIB_PATH.."SourceLib.lua"
@@ -46,7 +34,7 @@ end
 
 function OnLoad()
 ts = TargetSelector(TARGET_LESS_CAST_PRIORITY,315)
-m = scriptConfig("Wobble Wukong", "wobblewukong")
+m = scriptConfig("DEADSERIES - WUKONG", "deadwukong")
 VP = VPrediction()
 sow = SOW(VP)
 sow:RegisterAfterAttackCallback(hydra)
@@ -73,12 +61,13 @@ m.draws:addParam("drawr", "Draw R range", SCRIPT_PARAM_ONOFF, false)
 
 m:addParam("combokey", "Combo", SCRIPT_PARAM_ONKEYDOWN, false, 32)
 m:addParam("magnet", "Meele Magnet", SCRIPT_PARAM_ONOFF, true)
+m:addParam("target", "Left Click Target Selection", SCRIPT_PARAM_ONOFF, true)
 
 m:addSubMenu("Orbwalker", "orbwalk")
 sow:LoadToMenu(m.orbwalk)
 m:addTS(ts)
-ts.name = "Wobble"
-PrintChat ("<font color='#00BCFF'>Wobble Wukong v1.3 by DeadDevil2 Loaded! </font>")
+ts.name = "Selection"
+PrintChat ("<font color='#00BCFF'>DEADSERIES - WUKONG LOADED!</font>")
 end
 
 function OnGainBuff(unit, buff)
@@ -227,37 +216,39 @@ function CST()
 end
 
 function OnWndMsg(Msg, Key)
-    if Msg == WM_LBUTTONDOWN then
-        local minD = 10
-        local starget = nil
-        for i, enemy in ipairs(GetEnemyHeroes()) do
-            if ValidTarget(enemy) then
-                if GetDistance(enemy, mousePos) <= minD or starget == nil then
-                    minD = GetDistance(enemy, mousePos)
-                    starget = enemy
+    if m.target then
+        if Msg == WM_LBUTTONDOWN then
+            local minD = 10
+            local starget = nil
+            for i, enemy in ipairs(GetEnemyHeroes()) do
+                if ValidTarget(enemy) then
+                    if GetDistance(enemy, mousePos) <= minD or starget == nil then
+                        minD = GetDistance(enemy, mousePos)
+                        starget = enemy
+                    end
                 end
-            end
-        end     
-        if starget and minD < 500 then
-            if selectedTar and starget.charName == selectedTar.charName then
-                selectedTar = nil
-                print("<font color=\"#FFFFFF\">Wukong: Target <b>UNSELECTED</b>: "..starget.charName.."</font>")
-            else
-                selectedTar = starget               
-                print("<font color=\"#FFFFFF\">Wukong: New target <b>selected</b>: "..starget.charName.."</font>")
+            end     
+            if starget and minD < 500 then
+                if selectedTar and starget.charName == selectedTar.charName then
+                    selectedTar = nil
+                    print("<font color=\"#FFFFFF\">Wukong: Target <b>UNSELECTED</b>: "..starget.charName.."</font>")
+                else
+                    selectedTar = starget               
+                    print("<font color=\"#FFFFFF\">Wukong: New target <b>selected</b>: "..starget.charName.."</font>")
+                end
             end
         end
     end
 end
 
 function OnDraw()
-if m.draws.drawq then
-    DrawCircle(myHero.x, myHero.y, myHero.z, 300, ARGB(255, 255, 255, 255))
-end
-if m.draws.drawe then
-    DrawCircle(myHero.x, myHero.y, myHero.z, 625, ARGB(255, 255, 255, 255))
-end
-if m.draws.drawr then
-    DrawCircle(myHero.x, myHero.y, myHero.z, 315, ARGB(255, 255, 255, 255))
-end
+    if m.draws.drawq then
+        DrawCircle(myHero.x, myHero.y, myHero.z, 300, ARGB(255, 255, 255, 255))
+    end
+    if m.draws.drawe then
+        DrawCircle(myHero.x, myHero.y, myHero.z, 625, ARGB(255, 255, 255, 255))
+    end
+    if m.draws.drawr then
+        DrawCircle(myHero.x, myHero.y, myHero.z, 315, ARGB(255, 255, 255, 255))
+    end
 end
