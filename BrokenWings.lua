4a1df45651bba0c054be7d0acbe326538d824931
dd2repo <yannicklanzrath@googlemail.com>
2015-07-27 22:03:25 +0200

    Update BrokenWings.lua

--- BrokenWings.lua
+++ BrokenWings.lua
@@ -1,7 +1,7 @@
 --[[
   Script: Riven - Broken Wings
   Author: DD2
-  Version: BW514005
+  Version: BW514006
   
   Set _G.LoadSxAsDefault = false/true if you do not want/want to automatically load SxOrbWalker
 ]]
